8dfb5d0c617c244ef3d5415923c46c946dbe4dca
chunqi777 <LHY2274847317@outlook.com>
2022-04-18 18:52:41 +0800

    JDBC简单应用

--- src/JDBC/JdbcDemo.java
+++ src/JDBC/JdbcDemo.java
@@ -3,6 +3,7 @@ package JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JdbcDemo {
     public static void main(String[] args) throws Exception {
@@ -16,19 +17,52 @@ public class JdbcDemo {
         //获取连接
         Connection conn = DriverManager.getConnection(url, username, password);
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入名字:");
+        String name = sc.next();
+        System.out.print("请输入工资:");
+        int salary = sc.nextInt();
+
         //sql语句
         String sql1 = "UPDATE account SET money = 2000 WHERE id = 1";
+        String sql2 = "UPDATE account SET money = 3000 WHERE id = 2";
+        String sql3 = "insert into account (name,money) values('" + name + "'," + salary + ")";
 
         //获取执行sql的对象
         Statement stmt = conn.createStatement();
 
-        //执行sql语句
-        int count = stmt.executeUpdate(sql1);
+        try {
+            //开启事务
+            conn.setAutoCommit(false);
 
-        System.out.println("受影响的行数：" + count);
+            //执行sql语句
+            int count1 = stmt.executeUpdate(sql1);
+
+            System.out.println("受影响的行数：" + count1);
+
+            int count2 = stmt.executeUpdate(sql2);
+
+            System.out.println("受影响的行数：" + count2);
+
+            int count3 = stmt.executeUpdate(sql3);
+            if(count3>0){
+                System.out.println("修改成功！");
+            }else {
+                System.out.println("修改失败！");
+            }
+
+            //提交事务
+            conn.commit();
+        } catch (Exception e) {
+            //回滚事务
+            conn.rollback();
+
+            e.printStackTrace();
+        }
 
         //释放资源
         stmt.close();
         conn.close();
+        sc.close();
     }
 }
